61f54b5470b7faaa14bb09643d52f61fd2d8361d
charityvanb <charityvanb@gmail.com>
2018-10-31 11:34:28 -0600

    message list and single message working

--- src/App.js
+++ src/App.js
@@ -5,22 +5,44 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+<div class="row toolbar">
+  <div class="col-md-12">
+    <p class="pull-right">
+      <span class="badge badge">2</span>
+      unread messages
+    </p>
+
+    <button class="btn btn-default">
+      <i class={true ? "fa fa-check-square-o" : "fa fa-minus-square-o"}></i>
+    </button>
+
+    <button class="btn btn-default">
+      Mark As Read
+    </button>
+
+    <button class="btn btn-default">
+      Mark As Unread
+    </button>
+
+    <select class="form-control label-select">
+      <option>Apply label</option>
+      <option value="dev">dev</option>
+      <option value="personal">personal</option>
+      <option value="gschool">gschool</option>
+    </select>
+
+    <select class="form-control label-select">
+      <option>Remove label</option>
+      <option value="dev">dev</option>
+      <option value="personal">personal</option>
+      <option value="gschool">gschool</option>
+    </select>
+
+    <button class="btn btn-default">
+      <i class="fa fa-trash-o"></i>
+    </button>
+  </div>
+</div>
     );
   }
 }
